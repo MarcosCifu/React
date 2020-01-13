2b281f2b4eb3b2c371e90e5807e2183304c0c46b
Héctor Patricio Poblete Concha <hepoblet@gmail.com>
2020-01-13 16:29:58 -0300

    web

--- App.js
+++ App.js
@@ -4,7 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{
+          fontSize: 48
+        }}
+      >
+        PuliRed
+      </Text>
+      <Text>dev</Text>
     </View>
   );
 }
@@ -12,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
